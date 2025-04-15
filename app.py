bc48f05736f7bed21ca8619b1d28b7da93007c7e
mohamed ahmed <mohamedahmed46941@gmail.com>
2025-04-15 23:31:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -378,4 +378,4 @@ def cat_vs_num(cat, num, func):
 
 # Run app
 if __name__ == "__main__":
-    app.run_server(debug=False, host="0.0.0.0", port=8080)
+    app.run(debug=False, host="0.0.0.0", port=8080)
