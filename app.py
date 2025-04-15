764c3af5882137819e6043b1a1df1662aab7d103
mohamed ahmed <mohamedahmed46941@gmail.com>
2025-04-15 23:27:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -374,10 +374,8 @@ def cat_vs_num(cat, num, func):
     bar.update_yaxes(title=f"{func.capitalize()} {num} 💡", showgrid=True)
 
     return dcc.Graph(figure=box), dcc.Graph(figure=bar)
-if __name__ == "__main__":
-    app.run_server(debug=False, host="0.0.0.0", port=8080)
 
 
 # Run app
 if __name__ == "__main__":
-    app.run()
+    app.run_server(debug=False, host="0.0.0.0", port=8080)
